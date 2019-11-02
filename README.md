8db87d1cf2a98566ed5d793ae2d1880072b495e4
medbs <med.boulaares@gmail.com>
2019-11-02 15:41:12 +0100

    insert and get all todos

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # task-manager-be
+
+#### Add todo
+`curl -d "Title=task1" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:8080/api/v1/todos/
+`
+
+#### Get all todos
+`curl -X GET http://localhost:8080/api/v1/todos/
+`
--- main.go
+++ main.go
@@ -4,19 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	//"strconv"
 	"context"
 	"time"
 	"github.com/gin-gonic/gin"
 	"taskmanager/pkg/config"
-//	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Todo struct {
-	ID        int `json:"Field Int"`
+	Id        int  `json:"Field Int"`
 	Title     string `json:"Field Str"`
-	Completed bool `json:"Field Bool"`
 }
 
 var todosCollection *mongo.Collection
@@ -25,8 +24,7 @@ var todosCollection *mongo.Collection
 func init() {
 	 client := config.Connect()
    db := client.Database("blog")
-	 todosCollection := db.Collection("posts")
-	 _ = todosCollection
+	 todosCollection = db.Collection("posts")
 	}
 
 func main() {
@@ -36,7 +34,7 @@ func main() {
 	v1 := router.Group("/api/v1/todos")
 	{
 		v1.POST("/", createTodo)
-	//	v1.GET("/", fetchAllTodo)
+		v1.GET("/", fetchAllTodo)
 	//	v1.GET("/:id", fetchSingleTodo)
 	//	v1.PUT("/:id", updateTodo)
 	//	v1.DELETE("/:id", deleteTodo)
@@ -47,15 +45,14 @@ func main() {
 
 func createTodo(cxt *gin.Context) {
 
-	//title := cxt.PostForm("Title")
-
+	title := cxt.PostForm("Title")
 	ct, cancel := context.WithTimeout(context.Background(), 10*time.Second)
   defer cancel()
 
-	//var todo = Todo{1, title, false}
-	todo := Todo{1, "kek", true}
-	fmt.Println("" + todo.Title + " completed: " + strconv.FormatBool(todo.Completed))
-	_ , err := todosCollection.InsertOne(ct,&todo)
+	todo := Todo{1, title}
+	// fmt.Println(todosCollection)
+	 fmt.Println(todo)
+	_ , err := todosCollection.InsertOne(ct, todo)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -66,14 +63,27 @@ func createTodo(cxt *gin.Context) {
 	})
 }
 
-/*func fetchAllTodo(cxt *gin.Context) {
-	var todos []Todo
+func fetchAllTodo(cxt *gin.Context) {
+
+	var todos []*Todo
 	ct, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_ , err := todosCollection.Find(ct,nil).All(&todos)
+	 //err := todosCollection.Find(ct,nil).All(todos)
+	 cur,err := todosCollection.Find(ct, bson.D{{}})
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	for cur.Next(ct) {
+    // create a value into which the single document can be decoded
+    var todo Todo
+    err := cur.Decode(&todo)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    todos = append(todos, &todo)
+}
 
 	if len(todos) <= 0 {
 		cxt.JSON(http.StatusNotFound, gin.H{
@@ -87,71 +97,4 @@ func createTodo(cxt *gin.Context) {
 		"status": http.StatusOK,
 		"data":   todos,
 	})
-} */
-
-/*	func fetchSingleTodo(cxt *gin.Context) {
-	todo := Todo{}
-	id := bson.ObjectIdHex(cxt.Param("id"))
-	_ ,	err := todosCollection.FindId(id).One(&todo)
-
-	if err != nil || todo == (Todo{}) {
-		fmt.Println("Error: " + err.Error())
-		cxt.JSON(http.StatusNotFound, gin.H{
-			"status":  http.StatusNotFound,
-			"message": "todo not found",
-		})
-		return
-	}
-
-	cxt.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"data":   todo,
-	})
-} */
-
-/*func updateTodo(cxt *gin.Context) {
-	id := bson.ObjectIdHex(cxt.Param("id"))
-	title := cxt.PostForm("title")
-	completed, _ := strconv.ParseBool(cxt.PostForm("completed"))
-
-	_,err := todosCollection.UpdateOne(id, bson.M{"title": title, "completed": completed})
-
-	fmt.Printf("completed: %t\n\n", completed)
-
-	if err != nil {
-		fmt.Println("Error: " + err.Error())
-		cxt.JSON(http.StatusNotFound, gin.H{
-			"status":  http.StatusNotFound,
-			"message": "todo not found",
-		})
-		return
-	}
-
-	cxt.JSON(http.StatusOK, gin.H{
-		"status":  http.StatusOK,
-		"message": "Todo updated successfully!",
-	})
-} */
-
-/* func deleteTodo(cxt *gin.Context) {
-	id := bson.ObjectIdHex(cxt.Param("id"))
-
-	fmt.Printf("id: %v", id)
-
-	_,err := todosCollection.RemoveId(id)
-
-	if err != nil {
-		fmt.Println("Error: " + err.Error())
-		cxt.JSON(http.StatusNotFound, gin.H{
-			"status":  http.StatusNotFound,
-			"message": "todo not found",
-		})
-		return
-	}
-
-	cxt.JSON(http.StatusOK, gin.H{
-		"status":  http.StatusOK,
-		"message": "Todo deleted successfully!",
-	})
-
-} */
+}
